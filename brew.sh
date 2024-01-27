cc20ae8d3b4a52bd5a00a04486ea9e3092ed5beb
gummipunkt <github@gummipunkt.eu>
2024-01-27 20:08:12 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -23,8 +23,8 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
+#brew install bash
+#brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
@@ -39,18 +39,18 @@ brew install wget --with-iri
 brew install gnupg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
+#brew install vim --with-override-system-vi
 brew install grep
-brew install openssh
-brew install screen
-brew install php
+#brew install openssh
+#brew install screen
+#brew install php
 brew install gmp
 
 # Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+#brew tap bramstein/webfonttools
+#brew install sfnt2woff
+#brew install sfnt2woff-zopfli
+#brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
 brew install aircrack-ng
@@ -81,8 +81,8 @@ brew install xz
 # Install other useful binaries.
 brew install ack
 #brew install exiv2
-brew install git
-brew install git-lfs
+#brew install git
+#brew install git-lfs
 brew install gs
 brew install imagemagick --with-webp
 brew install lua
@@ -92,7 +92,7 @@ brew install pigz
 brew install pv
 brew install rename
 brew install rlwrap
-brew install ssh-copy-id
+#brew install ssh-copy-id
 brew install tree
 brew install vbindiff
 brew install zopfli
